94f7e6aaf25175e3a32cc222e083375158349639
Betenshi <82849337+Betenshi@users.noreply.github.com>
2021-11-09 12:41:01 +0100

    form

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,16 +1,33 @@
+<<<<<<< HEAD
 import './App.css';
+=======
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./App.css";
+
+
+
+
+>>>>>>> 070d2403e6114512d6168f7084a92685bd1c2e6d
 
 import Navbar from './components/Navbar';
 // import Routes from './components/Routes';
 
 
 function App() {
+<<<<<<< HEAD
     return (
         <div className="App">
       <h1>Educational Platform</h1>
 
       <Navbar />
       
+=======
+  return (
+    <div className="App">
+      <h1>TeachMe</h1>
+    
+>>>>>>> 070d2403e6114512d6168f7084a92685bd1c2e6d
     </div>
     )
 }
--- Client/src/components/SignupForm.js
+++ Client/src/components/SignupForm.js
@@ -1,36 +1,62 @@
-import React from 'react'
-import Form from 'react-bootstrap';
-function SignupForm() {
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+
+
+function SignupForm(props) {
+  const[userName, setUsername] = useState('');
+
+function handleChange(event) {
+  setUsername(event.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  props.submitCb(userName);
+  setUsername(''); 
+}
+
+
+
 return (
-<div>
-<Form>
+<div className="AddNewUser">
+  <h2>New Account Registration</h2>
+  <Form onSubmit={handleSubmit}>
      <Row className="mb-3">
          <Form.Group className="mb-3" controlId="formGridAddress1">
          <Form.Label>Your Name</Form.Label>
-         <Form.Control placeholder="insert your name..." />
+         <Form.Control placeholder="Insert your name..." />
          </Form.Group>
+
          <Form.Group as={Col} controlId="formGridEmail">
          <Form.Label>Email</Form.Label>
          <Form.Control type="email" placeholder="Enter email" />
          </Form.Group>
+
+
          <Form.Group as={Col} controlId="formGridPassword">
          <Form.Label>Password</Form.Label>
          <Form.Control type="password" placeholder="Password" />
         </Form.Group>
     </Row>
+ 
+ 
     <Form.Group as={Col} controlId="formGridState">
       <Form.Label>State</Form.Label>
       <Form.Select defaultValue="I am a...">
-        <option>Choose...</option>
         <option>Teacher</option>
         <option>Student</option>
       </Form.Select>
     </Form.Group>
-  <Button variant="primary" type="submit">
+
+   
+    
+  <Button variant="primary" type="submit" onClick={handleSubmit}>
     Submit
   </Button>
 </Form>
+            
 </div>
     )
 }
+
 export default SignupForm
