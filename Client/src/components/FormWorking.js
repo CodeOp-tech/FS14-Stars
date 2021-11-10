e685dff9f9f61559fc5d0cd9c430b9872f1e2ec6
unknown <mareragama@gmail.com>
2021-11-10 14:28:08 +0000

    Working on form

--- Client/src/components/FormWorking.js
+++ Client/src/components/FormWorking.js
@@ -1,84 +1,66 @@
 import React, {useState, useEffect} from "react";
 
 function Form(){
- const [exObjs, setExObjs] = useState([{sentence: "", options: "", answer: ""}]); 
- const [guesses, setGuesses] = useState({});
- const [showAnswer, setShowAnswer] = useState(false); 
 
-
-const getExObjs = () => {
-  fetch("/exercises")
-    .then(res => res.json())
-    .then(json => {
-      setExObjs(json);
-      
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-useEffect(() => {
-  getExObjs();
-}, []);
-
-
-function handleSubmit(e){
-  e.preventDefault(e);
-  showFunction()
-};
-
-function handleChange(e){
-  setGuesses({ ...guesses, [e.target.name]: e.target.value});
-  console.log(guesses)
-  //saves the changed menu value into the guesses obj 
-}
-
-const showFunction = (showAnswer) => {
-  setShowAnswer(true)
-} 
-
-
-  function buildAnswer(exObject){
-    console.log(guesses[exObject.id]);
-    if (exObject.answer === guesses[exObject.id]){
-      return "Correct!"
-    } else {
-      return `Answer: ${exObject.answer}`
-    }
+ const [exObjs, setExObjs] = useState ([{sentence: "", options: "", answer: ""}]);
+ const [guesses, setGuesses] = useState ({});
+ const [showAnswer, setShowAnswer] = useState (false);
+ //const to store exercise objects when i get them from server 
+ //const to save the guesses 
+ //const to store state to see whether or not the exercise has been submitted 
+ 
+ const getExObjs = () => {
+    fetch("/exercises") //need to change the fetch request 
+      .then(res => res.json())
+      .then(json => {
+        setExObjs(json);
+        
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
+ //use effect with a get fetch to get the forms 
+ //dont think there is a get for this yet but not sure 
 
-  function buildSentence(exObject) {
-  // Split options into array of preps
-  let prepArr = exObject.options.split(", ");
+    
+ //handle submit 
+//prevent default 
+//pass the data to the parent - rebecca view
+//reset form data 
 
-  // Convert/map arr of strings into array of <option> elements
-  let optArr = prepArr.map((elem) => (
-    <option value={elem}>{elem}</option>
-    ))
-  
-  // Split sentence on <menu>, giving you the parts before & after
-  let parts = exObject.sentence.split("<menu>");
+ //handle change 
+ //e.target.value 
+ //save them in the guesses state - guesses set guesses 
 
 
-  let sentence = (
-      <div className="sentence"> 
-          {parts[0]}
-          <select id={'ex-'+exObject.id} name={exObject.id} onChange={handleChange} required>
-              <option value="">choose one...</option>
-              {optArr}
-          </select>
-          {parts[1]}
-          <div className="showAnswer">
-          {
-            showAnswer && buildAnswer(exObject)
-          }
-            </div>
-      </div>
-  );
+//theres another one somewhere which checks to see if the form being submitted is true - 
+//not sure where this goes 
 
-  return sentence;
-};
+
+
+//return section
+//the form itself 
+//container
+//styling / offset 
+
+
+//build sentence function 
+//split the sentence on <menu> 
+//select, options etc 
+//
+
+//map the sentence parts 
+
+//show answers && i think the boolean to see if theyve submitted the form?
+//if the boolean is correct then it should map the answers in the same one 
+
+//compare answers function 
+
+//handle submit - call handle submit function 
+//on change - call on change function 
+
+
 
 
   return (
@@ -86,31 +68,7 @@ const showFunction = (showAnswer) => {
       
       <div className="container">
          
-         <div className="card">
-          <div className="card-header" style={{backgroundColor: "#cce6ff"}}>
-              <h1>Prepositions</h1>
-          </div>
-          <div className="card-body">
-               <h4 className="card-title">Exercise One</h4>       
-          </div>
-          </div>
-          <div className="card">
-          <form onSubmit={handleSubmit}>
-            
-          <ol>
-            {
-            exObjs.map(e => (
-            <li className="mb-2" key={e.id}>
-                {buildSentence(e)}
-            </li>
-            ))
-            }
-          </ol>
-          
-              <button type="submit" className="btn btn-primary">Get Answers</button>
-          </form>
-          </div>
-      </div>
+       </div>
       
     </div>
     )
--- Client/src/views/RebeccaGView.js
+++ Client/src/views/RebeccaGView.js
@@ -1,10 +1,16 @@
 import React from 'react'
 // import { View, Text } from 'react-native'
 
+//put <Form/> when its ready to display something 
+
 function RebeccaGView () {
+
+//i think all this needs to display is the form 
+
     return (
         <div>
-            <h2>Rebecca G</h2>
+            <h2>It's me!</h2>
+            
         </div>
     )
 }
