d81355c80f9e9cc24872549a3e973ddf0549a193
unknown <mareragama@gmail.com>
2021-11-17 11:34:41 +0000

    Updated files

--- Client/src/views/RebeccaGView.js
+++ Client/src/views/RebeccaGView.js
@@ -1,36 +1,20 @@
 import React, {useState, useEffect} from 'react'
+import ExerciseItems from '../components/ExerciseItems.js';
 // import { View, Text } from 'react-native'
-import AddExercise from '../components/AddExercise.js';
 
 
-//return <AddExercise/>
+
+
 function RebeccaGView () {
 
-const [exercise, setExercise] = useState({});
-let [input, setInput] = useState({});
-// //need to have a new one that lets it add more exercises 
+const [exercise, setExercise] = useState(null);
 
-
-
-const handleAddExercise = (newEx) => { 
-setExercise((prevEx) => [...prevEx, newEx]); 
-//console.log(newEx);
-};
-
-//create an add exercise function 
-//submitCb={(exercise) => addExercise(exercise)}
-// addExercise={(newEx) => handleAddExercise(newEx)}
 
     return (
-        <div> 
-            <AddExercise addExercise={(newEx) => handleAddExercise(newEx)}/>
+        <div>
+            <ExerciseItems/>
         </div>
     )
 }
 
 export default RebeccaGView;
-
-//i can get rid of exerciseitems and replace with teachersexercises when im ready to test it 
-
-
-
