687f49b7c4d45059b992c0942857f02600d190e4
Jim Rudolf <rudolf@jbrcodes.com>
2021-11-17 16:24:37 +0100

    Add <StudentScores />

--- Client/src/views/DeniseBView.js
+++ Client/src/views/DeniseBView.js
@@ -1,22 +1,15 @@
+import React from 'react'
+import StudentScores from '../components/StudentScores';
 
 
 
 
+function DeniseBView () {
+    return (
+        <div>
+            <StudentScores userId={3} />
+        </div>
+    )
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default DeniseBView;
